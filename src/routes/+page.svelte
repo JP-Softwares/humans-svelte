e7854a2bbf07427e4353165af92c8865425ecd46
Geydson <geydson87@gmail.com>
2024-02-17 23:21:31 -0300

    campo de digitar e botão de enviar Ok
    Criado o campo de digitar o texto e o botão de enviar que fica dentro desse campo.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script>
-	
+	function enviarMensagem() {
+		let mensagem = jQuery(".input-chat").val();
+
+		if(mensagem != "") {
+			jQuery(".input-chat").val(""); //Limpa o campo
+
+			//console.log(mensagem);
+		}
+	}
 </script>
 
 <svelte:head>
@@ -7,6 +15,42 @@
 	<meta name="description" content="" />
 </svelte:head>
 
-<section>
-	
-</section>
+<div class="chat-content">
+	<div>
+		<form on:submit={enviarMensagem}>
+			<div>
+				<div class="chat-box">
+					<input class="input input-chat" type="text">
+
+					<input class="button button-icon material-icons" type="submit" value="&#xe163;"/>
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
+
+<style>
+	.chat-content {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column-reverse;
+		flex: 1;
+	}
+
+	.chat-box {
+		display: flex;
+		flex-direction: row-reverse;
+	}
+
+	.button-icon {
+		position: absolute;
+		border: none;
+		background-color: #00000000;
+		color: #000;
+	}
+
+	.button-icon:hover {
+		color: #413434;
+	}
+</style>
